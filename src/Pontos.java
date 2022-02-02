3f9034067b1344580fe13509183dce81872bce53
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-02-02 16:59:55 -0300

    Criando uma classe para os pontos de localização do alvo (composição)

---
+++ src/Pontos.java
@@ -0,0 +1,26 @@
+public class Pontos {
+
+    private int x;
+    private int y;
+
+    public Pontos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
