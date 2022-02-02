3e0ef92fe96f24974e3968fc4fdd4dbe6a121b0e
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-02-02 12:34:23 -0300

    Testando a aplicação

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Interface game = new Interface();
+        game.run();
+
     }
 }
